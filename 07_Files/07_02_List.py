366a65f233c78e42a2f57979daeb83ebc988888e
Allie Green <allie_green@proton.me>
2023-01-28 23:31:25 +0100

    added 07_02_List.py version

---
+++ 07_Files/07_02_List.py
@@ -0,0 +1,17 @@
+f_name = input("Enter a file name: ")
+f_handle = open(f_name)
+
+count= 0
+all_floats = list()
+
+for line in f_handle:
+	if line.startswith("X-DSPAM-Confidence: "):
+		
+		f_num = float(line[20:])
+		all_floats.append(f_num)
+		count +=1
+
+average_spam = sum(all_floats) / count
+print(count)
+print("Average spam confidence:",average_spam)	
+#mbox-short.txt
