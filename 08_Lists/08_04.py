de7f813843b0e190f8c71e6d9ad520dfc93673ba
Allie Green <allie_green@proton.me>
2023-04-11 11:07:21 +0200

    Syntax issue fixed.

--- 08_Lists/08_04.py
+++ 08_Lists/08_04.py
@@ -1,8 +1,5 @@
 """
 8.4. Find all unique words in a file.
-
-* List all unique words, sorted in alphabetical order, that are stored in a file romeo.txt
-* Create a list of unique words, which will contain the final result.
 """
 f_name = input('Enter file: ')
 f_handle = open(f_name)
@@ -12,8 +9,8 @@ for line in f_handle:
     words = line.split()
     for word in words:
         x = word in unique_words
-    if x == True:
-        continue
-unique_words.append(word)
+        if x == False:
+            unique_words.append(word)
+
 unique_words.sort()
 print(unique_words)
