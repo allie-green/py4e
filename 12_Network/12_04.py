f4de844c57e370401aabd16e37cb2b6b7dfa4340
Allie Green <allie_green@proton.me>
2023-04-16 19:49:01 +0200

    Using the urllib library.

---
+++ 12_Network/12_04.py
@@ -0,0 +1,19 @@
+import urllib.request
+import re
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+count = 0
+url = 'https://www.python.org/'
+html = urllib.request.urlopen(url, context=ctx).read()
+paragraphs = re.findall('<p>.*?</p>'.encode(), html)
+
+for p in paragraphs:
+    count += 1
+    # print(p.decode())
+
+print(f'The are {count} paragraphs in this website.')
